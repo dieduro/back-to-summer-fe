b919101602f2a2527a29400aab4bc5e9f021f3d3
Diego Duro <diegomduro@gmail.com>
2021-11-05 12:53:49 -0300

    Add & Delete users buttons only in DEV env

--- components/CreateTrivia.js
+++ components/CreateTrivia.js
@@ -3,17 +3,12 @@ import Button from "../ui/Button";
 import Heading from "../ui/Heading";
 import QuestionsForm from "./QuestionsForm";
 
-const dev = process.env.NODE_ENV === 'development';
-const BASE_URL = dev ? 'http://localhost:3000' : 'https://back-to-summer.vercel.app';
-console.log("env: ", process.env.NODE_ENV);
-console.log("BASE_URL: ", BASE_URL);
-
 const registerUsers = async () => {
-  await fetch(`${BASE_URL}/api/process-users-sheet`).then(res => {console.log(res)})
+  await fetch('/api/process-users-sheet').then(res => {console.log(res)})
 }
 
 const deleteUsers = async () => {
-  await fetch(`${BASE_URL}/api/delete-users`).then(res => {console.log(res)})
+  await fetch('/api/delete-users').then(res => {console.log(res)})
 }
 const CreateTrivia = ({ questions }) => {
   return (
@@ -24,8 +19,14 @@ const CreateTrivia = ({ questions }) => {
         </Heading>
       </div>
       <QuestionsForm questions={questions} />
-      <Button onClick={e => registerUsers()}>Registrar test users</Button>
-      <Button onClick={e => deleteUsers()}>Borrar test users</Button>
+      {
+        process.env.NODE_ENV === 'development' &&
+        <>
+          <Button onClick={e => registerUsers()}>Registrar test users</Button>
+          <Button onClick={e => deleteUsers()}>Borrar test users</Button>
+        </>
+      }
+      
     </div>
   );
 };
